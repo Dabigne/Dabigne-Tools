eeabef74bfa676ecd9ca171b4f35c5d8e7b8a729
Jérôme Bignon <iphonejeromebignon@gmail.com>
2025-09-09 01:48:32 +0200

    Dynamic module loading

--- Application/App.axaml.cs
+++ Application/App.axaml.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Application.Core;
 using Application.Core.Interfaces.Services;
 using Application.Core.Services;
 using Application.Core.Utils;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Application.ViewModels;
 using Application.Views;
-using Autofac;
-using Module.MangaDownload;
 
 namespace Application;
 
@@ -39,9 +40,23 @@ public sealed class App : Avalonia.Application
         var modules = new List<Autofac.Module>
         {
             new CoreModule(),
-            new ApplicationModule(),
-            new MangaDownloadModule()
+            new ApplicationModule()
         };
+
+        var dynamicModules = Directory.GetFiles("Modules")
+            .Where(f => f.EndsWith(".dll"))
+            .ToList();
+        foreach (var module in dynamicModules)
+        {
+            var assembly = Assembly.LoadFrom(module);
+            var types = assembly.GetTypes();
+            var moduleTypes = types.Where(t => t.BaseType == typeof(Autofac.Module));
+            foreach (var moduleType in moduleTypes)
+            {
+                modules.Add((Activator.CreateInstance(moduleType) as Autofac.Module)!);
+            }
+        }
+        
         _instanceProvider.Start(modules);
     }
 }
