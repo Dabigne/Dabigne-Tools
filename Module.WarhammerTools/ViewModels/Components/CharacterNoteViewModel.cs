b0197486b1714c08cc24f05868705dfea63369a9
Jérôme Bignon <iphonejeromebignon@gmail.com>
2025-11-24 16:11:00 +0100

    Mise en place du mode de visualisation des notes

--- Module.WarhammerTools/ViewModels/Components/CharacterNoteViewModel.cs
+++ Module.WarhammerTools/ViewModels/Components/CharacterNoteViewModel.cs
@@ -1,7 +1,15 @@
+using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Module.WarhammerTools.Models;
 
 namespace Module.WarhammerTools.ViewModels.Components;
+
+public enum NoteViewType
+{
+    Both,
+    View,
+    Edit
+}
 
 public partial class CharacterNoteViewModel : ObservableObject
 {
@@ -11,6 +19,42 @@ public partial class CharacterNoteViewModel : ObservableObject
     [ObservableProperty]
     private string _text = string.Empty;
     
+    [ObservableProperty]
+    private NoteViewType _viewType = NoteViewType.Both;
+    
+    [ObservableProperty] 
+    private GridLength _editionWidth = GridLength.Star;
+
+    [ObservableProperty] 
+    private GridLength _viewWidth = GridLength.Star;
+    
+    public IList<NoteViewType> ViewTypes { get; } 
+        = [NoteViewType.Both, NoteViewType.View, NoteViewType.Edit];
+
+    public CharacterNoteViewModel()
+    {
+        ViewType = ViewTypes[0];
+    }
+
+    partial void OnViewTypeChanged(NoteViewType value)
+    {
+        switch (value)
+        {
+            case NoteViewType.Both:
+                ViewWidth = new GridLength(0.5, GridUnitType.Star);
+                EditionWidth = new GridLength(0.5, GridUnitType.Star);
+                break;
+            case NoteViewType.View:
+                ViewWidth = new GridLength(1 , GridUnitType.Star);
+                EditionWidth = new GridLength(0, GridUnitType.Star);
+                break;
+            case NoteViewType.Edit:
+                ViewWidth = new GridLength(0, GridUnitType.Star);
+                EditionWidth = new GridLength(1, GridUnitType.Star);
+                break;
+        }
+    }
+
     public CharacterNoteViewModel SetModel(Note note)
     {
         Title = note.Title;
